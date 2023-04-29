6d72b796124035243c87d2f6e4500dedbda3b529
Gabis13 <gabriel.viecili@gmail.com>
2023-04-29 20:42:10 -0300

    Second Commit

--- main.py
+++ main.py
@@ -13,13 +13,16 @@ while True:
     print("(3) Para adicionar palavras e dicas")
     print("(4) Mostrar palavras e dicas")
     opcao = input()
+   
     if opcao == "0":
         break
+    
     elif opcao == "1":
         cor = int(input("Digite o número da cor desejada: "))
         mudarCor(cor)
         input("press enter to continue...")
         aguarde(2)
+    
     elif opcao == "2":
         arquivo = open("bd.forca", "r")
         dados = arquivo.readlines()
@@ -33,44 +36,40 @@ while True:
             except:
                 palavra = dados[0]
             limparTela()
-            print("processando...")    
+            print("Loading...")    
             aguarde(2)
             print("A palavra foi escolhida")
             aguarde(2)
             tamanho = len(palavra)
             print("_"*int(tamanho-1))
-            print("chute uma letra: ")
-            letra = input()
-
+            letra = input("Digite uma letra: ")
 
     elif opcao == "3":
         print("Informe a Nova Palavra, e suas respectivas 3 dicas")
         palavra = lerString("Palavra: ")
         dica1 = lerString("Dica nº 1: ")
         dica2 = lerString("Dica nº 2: ")
-        dica3 = lerString("Dica nº 3: ")
-        try:
-            arquivo = open("bd.forca","a")
-            arquivo.write("Palavra: "+ palavra + "\n" + "Dica1: " + dica1+" Dica2: "+dica2+ " Dica3: "+ dica3+ "\n") #tem que organizar melhor para aparecer uma lista de forma correta
-            arquivo.close
-            print("Palavra Adicionada com Sucesso! ")
-            aguarde(2)
-        except:
-            arquivo = open("bd.forca", "w")
-            arquivo.close
+        dica3 = lerString("Dica nº 3: ")    
+        while True:
+            try:
+                arquivo = open("bd.forca","a")
+                arquivo.writelines(["\nPalavra: "+ palavra, "\nDica1: " + dica1, "\nDica2: "+dica2, "\nDica3: "+ dica3,],"\n")   #tem que organizar melhor para aparecer uma lista de forma correta         
+                arquivo.close
+                print("Palavra Adicionada com Sucesso! ")
+                aguarde(2)
+            except:
+                arquivo = open("bd.forca", "w")
+                arquivo.close
+    
     elif opcao == "4":
         arquivo = open("bd.forca","r")
         dados = arquivo.read()
         print(dados)
-        #print("bd.forca","r")
-        #input("press enter to continue...")
         arquivo.close
         aguarde(5)
+   
     else:
         print("Opção Inválida!")
         input("press enter tot continue...")
         aguarde(2)
 print("Volte Sempre!")
-        
-#(str(palavra) +"-"+ str(dica1)+ "-"+ str(dica2)+"-" + str(dica3)+"-"+ "\n")
-#(palavra +"-"+ dica1+ "-"+ dica2+"-" + dica3+"-"+ "\n")
